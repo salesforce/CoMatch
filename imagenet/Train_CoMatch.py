efe6566ef8a2e8f7aa0b1c1a305d9148d337efd8
Junnan Li <junnan.li@salesforce.com>
2021-03-03 10:23:06 +0800

    Update Train_CoMatch.py

--- imagenet/Train_CoMatch.py
+++ imagenet/Train_CoMatch.py
@@ -1,3 +1,9 @@
+'''
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+'''
 import argparse
 import os
 import random
